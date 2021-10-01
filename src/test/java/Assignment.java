99950029660b607095c83374f3686d73004b9fcf
Dean <dean.klopsch@hotmail.ch>
2021-10-01 10:04:14 +0200

    init

---
+++ src/test/java/Assignment.java
@@ -0,0 +1,56 @@
+import com.codeborne.selenide.Configuration;
+import lombok.extern.slf4j.Slf4j;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+
+@Slf4j
+public class Assignment {
+
+    //Selenide documentation: https://selenide.org/documentation.html
+
+    @Test
+    public void testOne() {
+        Configuration.headless = false;
+        Configuration.holdBrowserOpen = true;
+        Configuration.browser = "chrome";
+
+        //TODO: go to https://www.google.com
+        open( "https://www.google.com");
+
+        //TODO: get rid of terms of service
+        $(byText("I agree")).click();
+
+        //TODO: ensure the existence of the searchbar
+        final var input = $("input");
+        input.should(exist);
+        //TODO: search for Manuel Grädel
+        input.sendKeys("Manuel Grädel");
+
+        //TODO: switch to picture tab
+        input.pressEnter();
+
+
+        //TODO: find the Picture from the akros website and click on it
+        $(byText("Images")).click();
+
+        //TODO: visit the akros website using the link on the picture
+        $(byText("Qualitätsmanagement")).click();
+
+        //TODO: accept cookies
+        switchTo().window(1);
+        final var acceptCookies = $("button#onetrust-accept-btn-handler").should(appear);
+        acceptCookies.click();
+
+        //TODO: scroll to the Testautomation text
+        $(byText("Testautomation")).scrollIntoView(true);
+
+        //TODO: take a 'nice' screenshot of Testautomation section. No sleeps.
+        acceptCookies.should(disappear);
+        screenshot("test");
+
+    }
+
+}
---
+++ src/test/java/splitAndOrder.java
@@ -0,0 +1,25 @@
+import lombok.extern.slf4j.Slf4j;
+import org.testng.annotations.Test;
+
+import java.util.*;
+
+@Slf4j
+public class splitAndOrder {
+
+    /**
+    Aufgabe:
+    Sortiere die Wörter nach ihrer Anzahl absteigend, so dass die Ausgabe folgendermassen aussieht:
+     3 x cat
+     2 x lars
+     1 x rose
+     1 x circus
+     1 x cauliflower
+     1 x bit
+    */
+    @Test
+    public void Assignment2() {
+        String words = "lars, cat, bit, cat, cauliflower, circus, rose, cat, lars";
+
+    }
+
+}
